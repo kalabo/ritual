af1d0794210e856d6ff38e7bc30739dd02fe213e
TurboProlog <markghirst@gmail.com>
2015-02-04 20:08:11 +0000

    Added support for available slots to imminent bookings SP

--- Ritual.Booking.Data/Scripts/GetImminentSessionBookings.sql
+++ Ritual.Booking.Data/Scripts/GetImminentSessionBookings.sql
@@ -1,4 +1,4 @@
-﻿/****** Object:  StoredProcedure [dbo].[GetImminentSessionBookings]    Script Date: 30/01/2015 21:09:54 ******/
+﻿/****** Object:  StoredProcedure [dbo].[GetImminentSessionBookings]    Script Date: 04/02/2015 20:07:15 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -6,9 +6,9 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 -- =============================================
--- Author:		Mark Hirst
--- Create date: 30 Jan 2015
--- Description:	Gets the bookings starting in the next 30 minutes
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
 -- =============================================
 CREATE PROCEDURE [dbo].[GetImminentSessionBookings]
 
@@ -18,7 +18,21 @@ CREATE PROCEDURE [dbo].[GetImminentSessionBookings]
 AS
 BEGIN
 
+	Declare @availableSlots int
+
+	select @availableSlots = AvailableSlots from Location where Id = @LocationId;
+
+	with Numbers_cte as (
+    select 1 as Number
+    union all
+    select Number + 1
+    from Numbers_cte
+    where Number < @availableSlots)
+,
+ Session_CTE as (
+
 	select 
+		ROW_NUMBER() over (order by SessionBooking.id ) as RowNr,
 		TimeSlot.Id as TimeSlotId,
 		SessionBooking.id as SessionBookingId,
 		MemberId,
@@ -34,12 +48,14 @@ BEGIN
 		Member 
 	on 
 		SessionBooking.MemberId = Member.Id
-	  
 	where 
 		SessionBooking.Date = CONVERT(date, @CurrentDateTime) and 
 		TimeSlot.StartTime between CONVERT(time, @CurrentDateTime) and DATEADD(mi, 300, CONVERT(time, @CurrentDateTime)) and
 		SessionBooking.LocationId = @LocationId and 
 		SessionBooking.BookingStateId = 1
+		)
+
+	select * from Session_CTE right join Numbers_cte on Numbers_cte.Number = Session_CTE.RowNr
 END
 
 GO
